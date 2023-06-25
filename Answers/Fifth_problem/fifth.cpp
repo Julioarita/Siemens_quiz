a1951a9fb01de2e9e2a8b911d95ce2820cb2e299
Julio_Mac <julioarita1989@gmail.com>
2023-06-25 20:38:36 -0300

    Performing a unit test for function ConcatRemove()

--- Answers/Fifth_problem/fifth.cpp
+++ Answers/Fifth_problem/fifth.cpp
@@ -1,27 +1,38 @@
+/****************************************************
+  Julio Arita 25/06/2023      
+        
+    Definition of our unit test    
+
+    THIS IS INCOMPLETE.                 
+****************************************************/
+
+
+
 #include <iostream>
 #include <cassert>
-#include "../fourth.cpp" // Assuming the function is defined in a separate header file
+#include "ConcatRemove.h" 
 using namespace std;
 
+
+// Performing unit tests with different cases
 void test_ConcatRemove() {
     char s1[] = "abcdefghijklmnropqstuvwxyz";
-    char t1[] = "abcdefghijklmnropqstuvwxyzssss";
+    char t1[] = "abcdefghijklmnropqstuv";
     int k1 = 33;
     assert(ConcatRemove(s1, t1, k1) == "Yes");
 
-    char s2[] = "abc";
+    char s2[] = "abcdtasdfa";
     char t2[] = "def";
     int k2 = 6;
-    assert(ConcatRemove(s2, t2, k2) == "Yes");
+    assert(ConcatRemove(s2, t2, k2) == "No");
 
-    char s3[] = "abc";
-    char t3[] = "def";
-    int k3 = 5;
+    char s3[] = "calisthenic";
+    char t3[] = "calisthen";
+    int k3 = 3;
     assert(ConcatRemove(s3, t3, k3) == "No");
 }
 
 int main() {
     test_ConcatRemove();
-    cout << "All tests passed." << endl;
     return 0;
 }
